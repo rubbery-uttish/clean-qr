9273b554039297d684deda3a5b47a0f3322bdbb3
Peter Mooney <peter.mooney90@gmail.com>
2015-09-19 13:51:47 -0400

    GET API for Arduino, basic home page

---
+++ server/api.js
@@ -0,0 +1,23 @@
+// Global Configuration
+var Api = new Restivus({
+	useDefaultAuth: false,
+	prettyJson: true,
+	apiPath: 'api/',
+	version: 'v1',
+	defaultHeaders: {
+		'Content-Type': 'application/json'
+	}
+});
+
+Api.addRoute('/task/:task_id', {
+	get: function() {
+		var task_id = this.urlParams.task_id;
+		return Tasks.findOne(task_id).balance;
+	},
+	post: function() {
+
+	},
+	put: function() {
+
+	}
+});
